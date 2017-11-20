70cc083f68acdf9297f570fa3d3eddcdf885ec16
prodam <prodam@debian>
2017-11-20 19:23:54 -0200

    teste

--- moveis.js
+++ moveis.js
@@ -1,13 +1,13 @@
 //
 
-function detectswipe(el,func) {
+function detectswipe(el,id) {
   swipe_det = new Object();
   swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
   var min_x = 30;  //min x swipe for horizontal swipe
   var max_x = 30;  //max x difference for vertical swipe
   var min_y = 50;  //min y swipe for vertical swipe
   var max_y = 60;  //max y difference for horizontal swipe
-  var direc = "";
+  var direc = 0;
   ele = document.getElementById(el);
   ele.addEventListener('touchstart',function(e){
     var t = e.touches[0];
@@ -23,19 +23,21 @@ function detectswipe(el,func) {
   ele.addEventListener('touchend',function(e){
     //horizontal detection
     if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y) && (swipe_det.eX > 0)))) {
-      if(swipe_det.eX > swipe_det.sX) direc = "r";
-      else direc = "l";
+      if(swipe_det.eX > swipe_det.sX) direc = 1;
+      else direc = 1;
     }
     //vertical detection
     else if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x) && (swipe_det.eY > 0)))) {
-      if(swipe_det.eY > swipe_det.sY) direc = "d";
-      else direc = "u";
+      if(swipe_det.eY > swipe_det.sY) direc = -1;
+      else direc = -1;
     }
 
-    if (direc != "") {
-      if(typeof func == 'function') func(el,direc);
-    }
-    direc = "";
+    //if (direc != "") {
+    //  if(typeof func == 'function') func(el,direc);
+    //}
+    plusSlides(direc,id)
+    
+    direc = 0;
     swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
   },false);  
 }
@@ -59,7 +61,7 @@ function addModal(id,nimgs) {
 	mod.innerHTML=html;
 	document.body.appendChild(mod);
 	
-	detectswipe(mod.id,plusSlides);
+	detectswipe(mod.id,id);
 	
 }
 valortotal=0;
